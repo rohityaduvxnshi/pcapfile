bda59e80b02b5f3dc9bd3c353c911ae5293b5a9c
rohityaduvxnshi <rohityaduvxnshi@gmail.com>
2026-05-04 07:41:13 +0530

    Replace temporary main with Qt entry point

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,1 +1,10 @@
-temporary
+#include <QApplication>
+#include "MainWindow.h"
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
+}
