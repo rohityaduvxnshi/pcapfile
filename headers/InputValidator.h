6d0195cc9209e277c7fd0a24a14d8b01988b3665
rohityaduvxnshi <rohityaduvxnshi@gmail.com>
2026-05-04 07:24:32 +0530

    Add input validator header

---
+++ headers/InputValidator.h
@@ -0,0 +1,23 @@
+#ifndef INPUTVALIDATOR_H
+#define INPUTVALIDATOR_H
+
+#include "AppTypes.h"
+
+#include <QString>
+#include <QList>
+
+class InputValidator
+{
+public:
+    static bool validateFilePath(const QString& filePath, QString& errorMessage);
+    static bool validatePortText(const QString& portText, int& port, QString& errorMessage);
+    static bool validatePortValue(int port, QString& errorMessage);
+    static bool validateField(const QString& name,
+                              const QString& byteText,
+                              const QString& lengthText,
+                              const QString& resolutionText,
+                              QString& errorMessage);
+    static bool validateFields(const QList<FieldDefinition>& fields, QString& errorMessage);
+};
+
+#endif // INPUTVALIDATOR_H
