bff9449f6a6b5b7c55a3880b46364bfee24b89d8
rohityaduvxnshi <rohityaduvxnshi@gmail.com>
2026-05-15 16:22:13 +0530

    Add multi-filter UI controller declarations

--- headers/MainWindow.h
+++ headers/MainWindow.h
@@ -2,9 +2,14 @@
 #define MAINWINDOW_H
 
 #include "AppTypes.h"
+#include "FilterTypes.h"
 
+#include <QList>
 #include <QMainWindow>
 #include <QStringList>
+
+class QLineEdit;
+class QSpinBox;
 
 namespace Ui
 {
@@ -24,17 +29,35 @@ private slots:
     void onAddFieldClicked();
     void onRemoveFieldClicked();
     void onStartClicked();
+    void onFilterCountChanged(int count);
+    void onFilterModeChanged();
 
 private:
     QString tableText(int row, int column) const;
     bool collectFields(QList<FieldDefinition>& fields, QString& errorMessage) const;
+    bool collectFilterConfiguration(FilterConfiguration& config, QString& errorMessage) const;
+
     QStringList buildOutputHeaders(const QList<FieldDefinition>& fields) const;
+    QStringList buildPreviewHeaders(const QList<FieldDefinition>& fields) const;
     void prepareOutputTable(const QStringList& headers);
     void appendPreviewRow(const QStringList& row);
+
+    void rebuildFilterInputs();
+    void clearPortFilterBoxes();
+    void clearHeaderFilterBoxes();
+    int matchingFilterIndex(const ParsedUdpPacket& parsed, const FilterConfiguration& config) const;
+
+    QString buildPartitionCsvPath(const QString& baseCsvPath,
+                                  const QString& modeText,
+                                  const QString& filterLabel) const;
+
     void setBusy(bool busy);
     void setStatus(const QString& message);
 
     Ui::MainWindow* ui;
+    QList<QSpinBox*> m_portFilterBoxes;
+    QList<QLineEdit*> m_headerFilterBoxes;
+
     static const int PREVIEW_ROW_LIMIT = 5000;
 };
 
