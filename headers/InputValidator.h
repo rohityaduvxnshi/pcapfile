5b99be82ba704d71166cef13d820def0098e85a9
rohityaduvxnshi <rohityaduvxnshi@gmail.com>
2026-05-15 16:20:59 +0530

    Add filter validation API

--- headers/InputValidator.h
+++ headers/InputValidator.h
@@ -2,9 +2,12 @@
 #define INPUTVALIDATOR_H
 
 #include "AppTypes.h"
+#include "FilterTypes.h"
 
-#include <QString>
+#include <QByteArray>
 #include <QList>
+#include <QString>
+#include <QStringList>
 
 class InputValidator
 {
@@ -19,6 +22,28 @@ public:
                               const QString& resolutionText,
                               QString& errorMessage);
     static bool validateFields(const QList<FieldDefinition>& fields, QString& errorMessage);
+
+    static int minMessageFilterCount();
+    static int maxMessageFilterCount();
+    static bool validateMessageFilterCount(int count, QString& errorMessage);
+    static bool validateHeaderHexText(const QString& text,
+                                      QByteArray& bytesOut,
+                                      QString& labelOut,
+                                      QString& errorMessage);
+    static bool validatePortFilters(const QList<int>& ports,
+                                    QList<MessageFilter>& outFilters,
+                                    QString& errorMessage);
+    static bool validateHeaderFilters(int commonPort,
+                                      const QStringList& hexTexts,
+                                      QList<MessageFilter>& outFilters,
+                                      QString& errorMessage);
+    static bool validateFilterConfiguration(const FilterConfiguration& config,
+                                            QString& errorMessage);
+
+private:
+    static bool hexStringToBytes(const QString& normalizedHex,
+                                 QByteArray& bytesOut,
+                                 QString& errorMessage);
 };
 
 #endif // INPUTVALIDATOR_H
